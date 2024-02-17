8c9813d711d0cabbbdea04d4d2178798817d0fde
Silas Duarte <silas.duarte.h@gmail.com>
2024-02-16 23:20:49 -0600

    changes(fish) starship prompt, eza commands and yazi function

--- fish/config.fish
+++ fish/config.fish
@@ -2,25 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-
-function fish_prompt
-
-  function formatted_prompt_pwd
-    set -l home ~
-    set -l pwd (string replace --regex "$home" '~' $PWD)
-    echo (prompt_pwd --full-length-dirs 1)| string replace --regex --all '/' '/'
-  end
-  
-  set -l git_info (fish_git_prompt)
-  echo (set_color yellow)""(set_color green)(formatted_prompt_pwd)(set_color blue)$git_info(set_color yellow) (basename "$VIRTUAL_ENV") 
-  echo (set_color yellow)"\$ "
-end
-
-function fish_right_prompt
-
-end
-
-
 set fish_greeting ""
 set -x EDITOR nvim
 set -x PATH $PATH 
@@ -29,7 +10,7 @@ set -x PATH $PATH
 # ALIAS
 # --------------------
 alias l "eza --icons --grid"
-alias ll "eza -lh --icons --no-user --no-filesize --no-time"
+alias ll "eza --long --icons --no-user --no-time --group-directories-first"
 alias cat "bat --theme=gruvbox-dark"
 alias find fd
 alias ps procs
@@ -56,3 +37,17 @@ set --export PATH $BUN_INSTALL/bin $PATH
 
 # go bin
 set --export PATH $HOME/go/bin $PATH
+
+starship init fish | source
+
+
+# yazi navigation
+function ya
+	set tmp (mktemp -t "yazi-cwd.XXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
