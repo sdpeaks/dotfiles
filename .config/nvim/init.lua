28b6da97500ed825f3e3b3275407900bdc966cef
silas <silas.duarte.h@gmail.com>
2024-05-02 07:58:31 -0600

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,7 +5,7 @@ require("user.lazyvim")
 local dev = true
 if dev then
   -- darcubox-nvim
-  vim.opt.runtimepath:append("/home/sduarte/projects/darcubox-nvim")
+  vim.opt.runtimepath:append("/home/silas/src/darcubox-nvim")
 end
 
 vim.cmd.colorscheme("darcubox")
